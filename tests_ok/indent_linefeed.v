4a4eb8ce1bcd51cb63a9132a184969aeccdf5707
Michael McNamara <mac@verilog.com>
2006-01-06 00:23:42 +0000

    New test for indent issues.
    git-svn-id: file:///verilog-mode-svn/trunk@170 db5bb42b-8904-0410-88e9-aa7026cf2ec3

---
+++ tests_ok/indent_linefeed.v
@@ -0,0 +1,18 @@
+module foo;
+   input a,b;
+   
+   always @(a) begin
+      b <= #10 ~ a;
+   end
+endmodule // foo
+
+module bar;
+   // 
+   input a,b;
+   
+   always @(a) begin // 
+      b <= #10 ~  a;
+                               
+      a;
+   end
+endmodule // foo
