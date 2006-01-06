c241cee3c682224195e609d15f3ea859d56ada11
Michael McNamara <mac@verilog.com>
2006-01-06 21:00:08 +0000

    Insure directives are indented correctly.
    git-svn-id: file:///verilog-mode-svn/trunk@187 db5bb42b-8904-0410-88e9-aa7026cf2ec3

---
+++ tests_ok/indent_directives.v
@@ -0,0 +1,34 @@
+module foo;
+`ifdef LABEL_A
+   CHIP CPU (
+             .clkin(clkin),
+ `ifdef LABEL_B
+             .bclko(bclko),
+ `endif
+             .cmode(cmode),
+             );
+   input sysclk;
+ `ifdef LABEL_B
+   input bclko;
+ `endif
+   input cmode;
+`endif
+   
+// instead of:
+
+`ifdef LABEL_A
+   CHIP CPU (
+             .clkin(clkin),
+ `ifdef LABEL_B
+             .bclko(bclko),
+ `endif
+             .cmode(cmode),
+             );
+   input sysclk;
+ `ifdef LABEL_B
+   input bclko;
+ `endif
+   input cmode;
+`endif
+
+endmodule // foo
