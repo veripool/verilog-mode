8d7ed26e39ac457156e113e598d4771b290d6cf9
Michael McNamara <mac@verilog.com>
2006-04-20 21:35:25 +0000

    Test for properly indenting generate regions.
    git-svn-id: file:///verilog-mode-svn/trunk@223 db5bb42b-8904-0410-88e9-aa7026cf2ec3

---
+++ tests/indent_generate.v
@@ -0,0 +1,14 @@
+module test();
+   
+  reg [3:0] x;
+ 
+  genvar    i;
+  generate
+  for(i=0; i<4; i=i+1) begin:a
+  always @(*) begin
+  x[i] = 1;
+  end
+  wire y = 0; 
+  end
+  endgenerate
+endmodule // test
