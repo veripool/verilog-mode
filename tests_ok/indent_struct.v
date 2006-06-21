1bde7d8d32b5650bcbea85c8d3eb5a935aa6d4b3
Michael McNamara <mac@verilog.com>
2006-06-21 22:33:10 +0000

    Test for indentation of structs.
    git-svn-id: file:///verilog-mode-svn/trunk@248 db5bb42b-8904-0410-88e9-aa7026cf2ec3

---
+++ tests_ok/indent_struct.v
@@ -0,0 +1,36 @@
+module foo;
+   
+   a = { g + c; };
+   a = c;
+   
+   typedef struct 
+	   {
+	    reg    r;
+	    ahb_op_t         op;             // Read, write, etc.
+	    ahb_cycle_type_t cti;            // Cycle type for bursts
+	    ahb_incr_type_t  incr;           // Increment type (for bursts)
+	    bit 		  b;
+	    reg 		  r;
+	    ahb_thingy a;
+	    bit [31:2] 	  addr;           // Starting address
+	    bit [3:0] 	  byte_sel;       // Byte lane select
+	    int             len;            // Length of transfer
+	    bit [31:0] 	  data[0:7];      // Write data
+   } ahb_req_t;
+   
+   struct {
+	   reg f;
+	   xyzzy b;
+	   };
+endmodule // foo
+
+module foo (
+    input a;
+    input c;
+    output d;
+	    );
+   always @(a) g;
+   
+   
+
+endmodule // foo
