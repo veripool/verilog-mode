452ea7eb6a805fa73ba9da3dac24a5e463a17dd4
Michael McNamara <mac@verilog.com>
2006-09-15 18:02:55 +0000

    *** empty log message ***
    git-svn-id: file:///verilog-mode-svn/trunk@260 db5bb42b-8904-0410-88e9-aa7026cf2ec3

--- tests/indent_property.v
+++ tests/indent_property.v
@@ -6,21 +6,28 @@ module foo();
    always @(a) begin
       b;
    end
-   task t;
-      c;
-   endtask // t
+   task a (a);
+      begin
+	 a = f;
+      end
+   endtask // a
    
-   property p_1;
-      @(posedge clk) a |-> b;
-   endproperty
-   initial
-     d;
    
    property p_2;
       @(posedge clk) b |-> ##1 c;
    endproperty
-
+   property p_1;
+      @(posedge clk) a |-> b;
+   endproperty
+   
+   initial d;
+   
    ap_1 assert property (p_1);
    ap_2 assert property (p_2);
    
+   property p_lane_output_change_on_input_change;
+      @(negedge test_clk)
+        disable iff (ana_byp == 0)
+	  !$stable(lane_inputs) |-> !$stable(lane_outputs);
+   endproperty
 endmodule
