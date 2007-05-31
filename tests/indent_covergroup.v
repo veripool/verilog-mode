85dfe37f870a8a5e50effb7bd89a29a6fa7f20a9
Michael McNamara <mac@verilog.com>
2007-05-31 23:35:42 +0000

    Tests for covergroup, sequence.
    git-svn-id: file:///verilog-mode-svn/trunk@337 db5bb42b-8904-0410-88e9-aa7026cf2ec3

---
+++ tests/indent_covergroup.v
@@ -0,0 +1,46 @@
+module coverage;
+   enum { red, green, blue } color;
+   covergroup foo @(posedge clk);
+      c : coverpoint color;
+      c : coverpoint color;
+      c : coverpoint color;
+      c : coverpoint color;      
+   endgroup // foo
+   
+   foo 	  = bar;
+
+   sequence bar
+     b 	  = c;
+   endsequence // bar
+   j 	  = taskt;
+   function foo;
+      begin
+	 foo 	= 1;
+      end
+   endfunction // foo
+   
+   randsequence bar
+     b 		= c;
+   endsequence // bar
+
+   case (foo)
+     1: a;
+     2:b;
+   endcase // case (foo)
+
+   casex (foo)
+     1: a;
+     2:b;
+   endcase // case (foo)
+
+   casez (foo)
+     1: a;
+     2:b;
+   endcase // case (foo)
+
+   randcase (foo)
+     1: a;
+     2:b;
+   endcase // case (foo)
+   
+endmodule // coverage
