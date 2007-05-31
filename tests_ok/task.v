7e3b9a5d0a221182b45d80d8f3f9cef2b04d709f
Michael McNamara <mac@verilog.com>
2007-05-31 23:36:13 +0000

    Tests for task ports.
    git-svn-id: file:///verilog-mode-svn/trunk@339 db5bb42b-8904-0410-88e9-aa7026cf2ec3

---
+++ tests_ok/task.v
@@ -0,0 +1,14 @@
+module foo(input bit [3:0] def,
+    input bit         ghi,
+    input bit [1:0] jkl);
+   
+   task cba(input bit [3:0] a,
+    input b,
+	    c);
+   endtask // cba
+   task abc(input bit [3:0] def,
+    input bit         ghi,
+    input bit [1:0] jkl);
+      
+   endtask // abc
+endmodule // foo
