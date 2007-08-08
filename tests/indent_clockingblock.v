19d6a80be77040b9aa7453b0f167e0f6d5b04c57
Michael McNamara <mac@verilog.com>
2007-08-08 21:41:28 +0000

    Support clocking..endclocking blocks (indent, commenting).
    git-svn-id: file:///verilog-mode-svn/trunk@349 db5bb42b-8904-0410-88e9-aa7026cf2ec3

---
+++ tests/indent_clockingblock.v
@@ -0,0 +1,38 @@
+class mipsbfm_trans extends vmm_data;
+   static vmm_log log = new ("mipsbfm_trans", "class") ;
+   logic [31:0] addr, data, mask, op;
+   function new();
+      super.new(this.log);
+   endfunction: new
+endclass // mipsbfm_trans
+
+
+interface mipsbfm_if(input clk);
+   logic [31:0] data;
+   logic [31:0] addr;
+   logic [31:0] mask;
+   logic [31:0] op;
+   logic 	valid;
+   
+   clocking cb @(posedge clk);
+      output 	data;
+      output 	addr;
+      output 	mask;
+      output 	op;
+      output 	valid;
+   endclocking // cb
+
+endinterface // mipsbfm_if
+
+
+`vmm_channel(mipsbfm_trans)
+  
+  //--------------------------------------------------------------
+  // MIPS BFM Master Xactor Class
+  //--------------------------------------------------------------
+  
+  class mipsbfm_master extends vmm_xactor;
+// Transaction channels
+mipsbfm_trans_channel  in_chan ;
+
+endclass // mipsbfm_master
