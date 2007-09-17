ae02108ddbcd9de58f2a0e019257690efb70aace
Michael McNamara <mac@verilog.com>
2007-09-17 16:51:32 +0000

    git-svn-id: file:///verilog-mode-svn/trunk@353 db5bb42b-8904-0410-88e9-aa7026cf2ec3

--- tests_ok/indent_clockingblock.v
+++ tests_ok/indent_clockingblock.v
@@ -5,8 +5,7 @@ class mipsbfm_trans extends vmm_data;
       super.new(this.log);
    endfunction: new
 endclass // mipsbfm_trans
-
-
+   
 interface mipsbfm_if(input clk);
    logic [31:0] data;
    logic [31:0] addr;
@@ -23,16 +22,16 @@ interface mipsbfm_if(input clk);
    endclocking // cb
 
 endinterface // mipsbfm_if
+   
+   
+`vmm_channel(mipsbfm_trans) ;
 
+//--------------------------------------------------------------
+// MIPS BFM Master Xactor Class
+//--------------------------------------------------------------
 
-`vmm_channel(mipsbfm_trans)
-  
-  //--------------------------------------------------------------
-  // MIPS BFM Master Xactor Class
-  //--------------------------------------------------------------
-  
-  class mipsbfm_master extends vmm_xactor;
-// Transaction channels
-mipsbfm_trans_channel  in_chan ;
-
+class mipsbfm_master extends vmm_xactor;
+   // Transaction channels
+   mipsbfm_trans_channel  in_chan ;
+   
 endclass // mipsbfm_master
