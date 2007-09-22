a4bfd53766bd11db82fbebccfbd47a5c73cbf1eb
Michael McNamara <mac@verilog.com>
2007-09-22 00:07:49 +0000

    Tests for the below:
    1) Work to correctly indent coverpoints, cross and constraint definitions which
       use { } as does C, where as every other place in verilog begin end would be used.
       (Can you tell I am annoyed? :-)
    
    2) Fix the case where an infinite loop would occur if one was lining up expressions and defining a module that
       includes parameters in the port list
    
    
    git-svn-id: file:///verilog-mode-svn/trunk@358 db5bb42b-8904-0410-88e9-aa7026cf2ec3

---
+++ tests_ok/indent_constraint.v
@@ -0,0 +1,95 @@
+// my simple if else example, indented by verilog-mode
+if (x == 1) 
+ begin
+ test1 <= 1;
+ test2 <= 2;
+ end
+else
+ begin
+ test1 <= 2;
+ test2 <= 1;
+ end
+
+// code from IEEE spec, pg. 164
+class MyBus extends Bus;
+ rand AddrType atype;
+ constraint addr_range
+ {
+ (atype == low ) -> addr inside { [0 : 15] };
+ (atype == mid ) -> addr inside { [16 : 127]}; 
+ (atype == high) -> addr inside {[128 : 255]};
+ }
+endclass // MyBus
+
+// same example, with verilog mode indenting, Cexp indent = 3
+class MyBus extends Bus;
+ rand AddrType atype;
+ constraint addr_range
+ {
+ (atype == low ) -> addr inside { [0 : 15] };
+ (atype == mid ) -> addr inside { [16 : 127]}; 
+ (atype == high) -> addr inside {[128 : 255]};
+ }
+endclass // MyBus
+
+// same example, with verilog mode indenting, Cexp indent = 0
+class MyBus extends Bus;
+ rand AddrType atype;
+ constraint addr_range
+ {
+ (atype == low ) -> addr inside { [0 : 15] };
+ (atype == mid ) -> addr inside { [16 : 127]}; 
+ (atype == high) -> addr inside {[128 : 255]};
+ }
+endclass // MyBus
+
+// covergroup example from IEEE pg. 317
+covergroup cg @(posedge clk);
+ a: coverpoint v_a
+ {
+ bins a1 = { [0:63] };
+ bins a2 = { [64:127] };
+ bins a3 = { [128:191] };
+ bins a4 = { [192:255] };
+ }
+ b: coverpoint v_b
+ {
+ bins b1 = {0};
+ bins b2 = { [1:84] };
+ bins b3 = { [85:169] };
+ bins b4 = { [170:255] };
+ } 
+ c: cross a, b
+ {
+ bins c1 = ! binsof(a) intersect {[100:200]}; // 4 cross products
+ bins c2 = binsof(a.a2) || binsof(b.b2); // 7 cross products
+ bins c3 = binsof(a.a1) && binsof(b.b4); // 1 cross product
+ }
+endgroup
+
+// here is the same code with verilog-mode indenting
+// covergroup example from IEEE pg. 317
+covergroup cg @(posedge clk);
+ a: coverpoint v_a
+ {
+ bins a1 = { [0:63] };
+ bins a2 = { [64:127] };
+ bins a3 = { [128:191] };
+ bins a4 = { [192:255] };
+ }
+ 
+ b: coverpoint v_b
+ {
+ bins b1 = {0};
+ bins b2 = { [1:84] };
+ bins b3 = { [85:169] };
+ bins b4 = { [170:255] };
+ } 
+ c: cross a, b
+ {
+ bins c1 = ! binsof(a) intersect {[100:200]}; // 4 cross products
+ bins c2 = binsof(a.a2) || binsof(b.b2); // 7 cross products
+ bins c3 = binsof(a.a1) && binsof(b.b4); // 1 cross product
+ }
+endgroup
+
---
+++ tests_ok/indent_param.v
@@ -0,0 +1,14 @@
+module example_block
+  #(
+    parameter PARAM    = 1,
+    FOO 	       = 2,
+    BARFLUG 	       = 4,
+    G 		       = 5,
+    )
+   (// I/O
+    input     reset_n,
+    input     clk
+    input     a, b,
+    output reg c
+    );
+endmodule
