e640af213f2157972ca37625eec424e1ac4e0c78
Michael McNamara <mac@verilog.com>
2007-11-20 21:11:53 +0000

    *** empty log message ***
    git-svn-id: file:///verilog-mode-svn/trunk@376 db5bb42b-8904-0410-88e9-aa7026cf2ec3

---
+++ tests_ok/debug.v
@@ -0,0 +1,15 @@
+`include "some_macros.v"
+
+module z();
+  
+   $display("%t:", $time);
+   a 			      = b;
+   casfasdf 		      = d;
+   g 			      = r;
+   fgasdfasdfasdfasfdasfd    <= p;
+   gh 			     := h;
+   gf 			     <=g;
+   ssdf 		      = 5;
+   f 			      = zsfdsdf >= f;
+  
+endmodule
