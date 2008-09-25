22c2b18031e9756d58dc4102eb4fffcef33f4e46
Michael McNamara <mac@verilog.com>
2008-09-25 22:33:24 +0000

    Test for supporting autoindent of virtual function.
    git-svn-id: file:///verilog-mode-svn/trunk@441 db5bb42b-8904-0410-88e9-aa7026cf2ec3

--- tests/indent_function.v
+++ tests/indent_function.v
@@ -1,6 +1,21 @@
-class any_env extends vmm_env;
- virtual function void junk();
- integer foo;
-endclass // any_env
-
-virtual function void junk();
+module t;
+class C;
+   function int f();
+      f = 17;
+   endfunction
+   extern function int g();
+      virtual function int f();
+      a;
+endfunction // int
+   
+   integer foo;
+endclass // C
+   C a;
+   initial begin
+      $display("hello world");
+      $display("a of f is %d, g is %d", a.f(),a.g());
+   end
+   function int C::g();
+      g = 18;
+   endfunction
+endmodule
