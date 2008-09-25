426217d821e6cb36bfa33705635eee4f3fff8a12
Michael McNamara <mac@verilog.com>
2008-09-25 22:34:13 +0000

    Test for supporting autoindent of virtual function.
    git-svn-id: file:///verilog-mode-svn/trunk@442 db5bb42b-8904-0410-88e9-aa7026cf2ec3

--- tests/indent_task.v
+++ tests/indent_task.v
@@ -33,14 +33,47 @@ module foo;
 	$display("%t: %s - Requires >= 2 RX frames in order to measure performance", $time, myName);
       
    endtask // printRxPerformance
-   class a;
-      virtual function void foo();
-	 foo = 2;
-      endfunction // void
-      extern function void bar();
-      function fred();
-	 aaa;
-      endfunction // fred
-   endclass // a
    
 endmodule // foo
+
+class a;
+   virtual function void foo();
+      foo = 2;
+   endfunction // void
+   extern function void bar();
+   function fred();
+      aaa;
+   endfunction // fred
+   
+   task foo;
+   endtask // endtask
+   
+   virtual task foo;
+   endtask // endtask
+   
+   generate g;
+   endgenerate
+   
+   covergroup g;
+   endgroup // g
+   
+   property p;
+   endproperty
+   
+   sequence s;
+   endsequence // s
+   
+   clocking c;
+   endclocking // c
+   
+   function f;
+   endfunction //
+   
+   virtual function f;
+   endfunction //
+   
+   protected function f;
+   endfunction //
+   
+endclass // a
+
--- tests_ok/indent_task_func_decl.sv
+++ tests_ok/indent_task_func_decl.sv
@@ -1,6 +1,5 @@
 typedef class burst_drv;
    
-   
 class burst_drv extends vmm_xactor;
    
    int EXECUTING;
@@ -16,8 +15,53 @@ class burst_drv extends vmm_xactor;
    burst_xn_channel obs_chan;
    
    burst_xn tr_main; // Current transaction in main()
-   
+   /* could have [extern] [virtual] [protected|local] task [static|automatic|] name (); */
+   /* If extern -> then it is complete; other wise it is not complete */
+   /* class could have:
+    class c
+       extern virtual static protected task t ();
+    endclass
+    task declaration could have
+    task static t();
+    endtask
+    */
+   class c;
+   endclass // c
+   function f();
+      g;
+   endfunction // f
+   generate g;
+      /* a lot of stuff */
+   endgenerate
+   task t();
+      /**/
+      /**/            
+   endtask // t
+   protected virtual task pv_t();
+      /**/
+   endtask // pv_t
+
+   protected task p_t();
+      /* ACK*/
+   endtask // p_t
+   virtual task v_t();
+      /**/      
+   endtask // v_t
+   virtual protected task vp_t();
+      /**/
+   endtask // vp_t
+   protected virtual task pv_t();
+      /**/
+   endtask // pv_t
+   extern task e_t();
+   extern virtual task ev_t();
+   extern protected task ep_t();
+   extern protected virtual task epv_t();
    extern protected virtual task main();
+   generate g;
+      /**/
+   endgenerate
+   
    extern virtual function void reconfigure(burst_drv_cfg cfg);
    extern virtual function void reset_xactor(reset_e rst_type = SOFT_RST);
    extern virtual function new (
@@ -27,4 +71,19 @@ class burst_drv extends vmm_xactor;
 				burst_xn_channel in_chan       = null,
 				burst_xn_channel obs_chan      = null,
 				burst_xn         rx_factory    = null);
+   virtual task start();
+      super.start();
+      this.tx_dma.start();
+      this.rx_dma.start();
+   endtask // start
+   task start();
+      super.start();
+      this.tx_dma.start();
+      this.rx_dma.start();
+   endtask // start
+   task static start();
+      super.start();
+      this.tx_dma.start();
+      this.rx_dma.start();
+   endtask // static
 endclass : burst_drv
