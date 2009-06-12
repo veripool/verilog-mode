038d9eef7fe7b7b46c7d7d5ba2143c8ddbbb5e38
Michael McNamara <mac@verilog.com>
2009-06-12 06:17:45 +0000

    *** empty log message ***
    git-svn-id: file:///verilog-mode-svn/trunk@523 db5bb42b-8904-0410-88e9-aa7026cf2ec3

--- tests_ok/indent_clocking.v
+++ tests_ok/indent_clocking.v
@@ -1,18 +1,18 @@
 module t;
    default clocking @(posedge clk);
       begin
-	 a  = 8;
+         a  = 8;
       end
       property foo (a)
-	a   = b;
+        a   = b;
       endproperty
       cover property (prop) $display("**COVERAGE**");
       assert property (foo) a;
       assume property (bar) b;
       b1: assume property (bar) b;
-	 B2: assert property (foo) a;
-	    B2: cover property (foo) a;
-	       assume property (bar) b;	       
-	       a;
-   endclocking // Woops should be indent 3
+         B2: assert property (foo) a;
+            B2: cover property (foo) a;
+               assume property (bar) b;
+               a;
+            endclocking // Woops should be indent 3
 endmodule
