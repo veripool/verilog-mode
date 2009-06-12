ab5905ffde38ca7f212aa4e5ec19fc3af67c1509
Michael McNamara <mac@verilog.com>
2009-06-12 06:16:56 +0000

    General cleanup.
    git-svn-id: file:///verilog-mode-svn/trunk@521 db5bb42b-8904-0410-88e9-aa7026cf2ec3

--- 0test.el
+++ 0test.el
@@ -1,4 +1,5 @@
 ;; $Id$
+(defvar diff-flags "-c")
 ;;
 ;;   VERILOG_MODE_TEST_NO_INDENTS=1    # Disable indent checks
 ;;   VERILOG_MODE_TEST_FILE=filename   # Run only specified test
@@ -29,70 +30,73 @@
       (verilog-auto)))
     (message (concat file ": auto OK..."))
     
-    (message (concat file ": testing indent..."))
-
     (unless (getenv "VERILOG_MODE_TEST_NO_INDENTS")
+      (message (concat file ": testing indent..."))
       (save-excursion
 	(goto-char (point-min))
-	(setq ln 0)
-	(while (not (eobp))
-	  (message (format "%d" ln))
-	  ;;(message (format "%s : %d - indent" file ln))
-	  (electric-verilog-tab)
-	  ;;(message (format "%s : %d - pretty-expr" file ln))
-	  (verilog-pretty-expr t )
-	  ;;(message (format "%s : %d - pretty-declaration" file ln))
-	  (verilog-pretty-declarations t)
-	  (forward-line 1)
-	  (setq ln (1+ ln))
-	  ))
-      (message (concat file ": indents OK...")))
+	(let* ((ln 0))
+	  (while (not (eobp))
+	    (message (format "%d" ln))
+	    ;;(message (format "%s : %d - indent" file ln))
+	    (electric-verilog-tab)
+	    ;;(message (format "%s : %d - pretty-expr" file ln))
+	    (verilog-pretty-expr t )
+	    ;;(message (format "%s : %d - pretty-declaration" file ln))
+	    (verilog-pretty-declarations t)
+	    (forward-line 1)
+	    (setq ln (1+ ln))
+	    )))
+      (message (concat file ": indents OK..."))
+      
+;      (message (concat file ": testing auto endcomments..."))
+;      (verilog-label-be)
+      )
   
     (untabify (point-min) (point-max))
     
     (write-file (concat "../" temp-file))
     (kill-buffer nil))
   ;;
-  (diff-file file))
+  (diff-file file temp-file))
 
-(defun diff-file (file)
+(defun diff-file (file temp-file)
   (message (concat file ": running diff of " file " and tests_ok/" file ))
   (with-temp-buffer
-    (let* ((flags (if (getenv "VERILOG_MODE_TEST_NO_INDENTS") "-wBc" "-c"))
-	   (status
-            (call-process "diff" nil t t flags "--label" "GOLDEN_REFERENCE" (concat "tests_ok/" file) "--label" "CURRENT_BEHAVIOR" temp-file )))
+    (let* ((status
+	    (call-process "diff" nil t t diff-flags "--label" "GOLDEN_REFERENCE" (concat "tests_ok/" file) "--label" "CURRENT_BEHAVIOR" temp-file )))
       (cond ((not (equal status 0))
-             (message (concat "diff -c tests_ok/" file " " temp-file))
-             (message "***Golden Reference File\n---Generated Test File")
-             (message "%s" (buffer-string))
-             (message "To promote current to golden, in shell buffer hit newline anywhere in next line (^P RETURN):")
-             (message (concat "cp " temp-file " tests_ok/" file "; VERILOG_MODE_START_FILE=" file " " again ))
-             (error ""))
-            
-            (t
-             (message "Verified %s" file))))))
+	     (message (concat "diff -c tests_ok/" file " " temp-file))
+	     (message "***Golden Reference File\n---Generated Test File")
+	     (message "%s" (buffer-string))
+	     (message "To promote current to golden, in shell buffer hit newline anywhere in next line (^P RETURN):")
+	     (message (concat "cp " temp-file " tests_ok/" file "; VERILOG_MODE_START_FILE=" file " " again ))
+	     (error ""))
+	    
+	    (t
+	     (message "Verified %s" file))))))
 
 (defun verilog-test ()
   (let ((files (directory-files "tests"))
         (tests-run 0)
+	file cf
         )
     (when (getenv "VERILOG_MODE_TEST_FILE")
       (setq files (list (getenv "VERILOG_MODE_TEST_FILE")))
       (message "**** Only testing files in $VERILOG_MODE_TEST_FILE"))
     
     (when (getenv "VERILOG_MODE_START_FILE")
-      (setq startfiles (list (getenv "VERILOG_MODE_START_FILE")))
-      (setq startfile (car startfiles))
-      (message (concat "Staring from file " startfile))
-      (catch 'done
-        (while files
-          (setq file (car files))
-          (if (string-equal file startfile)
-              (progn
-                (message (concat "matched " file))
-                (throw 'done 0))
-            (progn
-              (setq files (cdr files)))))))
+      (let* ((startfiles (list (getenv "VERILOG_MODE_START_FILE")))
+	     (startfile (car startfiles)))
+	(message (concat "Staring from file " startfile))
+	(catch 'done
+	  (while files
+	    (setq file (car files))
+	    (if (string-equal file startfile)
+		(progn
+		  (message (concat "matched " file))
+		  (throw 'done 0))
+	      (progn
+		(setq files (cdr files))))))))
     
     (while files
       (setq file (car files))
@@ -152,6 +156,8 @@
   (setq running-on-xemacs (string-match "XEmacs" emacs-version))
   (setq make-backup-files nil)
   (setq-default make-backup-files nil)
+  (setq diff-flags (if (getenv "VERILOG_MODE_TEST_NO_INDENTS") "-wBc" "-c"))
+  ;;(setq verilog-auto-lineup 'all)
   (setq enable-local-variables t)
   (setq enable-local-eval t)
   (setq auto-mode-alist (cons  '("\\.v\\'" . verilog-mode) auto-mode-alist))
