9dce824ac84d0b6eefa46171250be39ace4cab20
Wilson Snyder <wsnyder@wsnyder.org>
2009-11-02 15:36:25 +0000

    New test.
    git-svn-id: file:///verilog-mode-svn/trunk@545 db5bb42b-8904-0410-88e9-aa7026cf2ec3

---
+++ tests/autoinst_mccoy.v
@@ -0,0 +1,33 @@
+module top
+
+  // no-connecting unused outputs of an interface and tying only inputs to gnd.
+
+  // Requested:
+  // 
+  //  /* xx AUTO_TEMPLATE (
+  //.TWI_\(.*\) @"(vl-dir (input)"    ({@"vl-width"{1'b0}}),
+  //.TWI_\(.*\) @"(vl-dir (output)"    (),
+  //);
+  //   */
+
+  /* xx AUTO_TEMPLATE (
+   .TWI_\(.*\) (@"(if (equal vl-dir \\"output\\") \\"\\" (concat vl-width \\"'b0\\"))"),
+   );
+   */
+
+   xx  XX (/*AUTOINST*/
+	   // Outputs
+	   .TWI_qb			(),			 // Templated
+	   // Inputs
+	   .clk				(clk),
+	   .TWI_ia			(1'b0),			 // Templated
+	   .TWI_iw			(16'b0));		 // Templated
+endmodule
+
+module xx
+   (input clk,
+
+    input  TWI_ia,
+    input  [15:0] TWI_iw,
+    output TWI_qb);
+endmodule
