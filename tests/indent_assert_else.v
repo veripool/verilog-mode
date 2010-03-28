e34dcbda51d0af15caf7d57d943503d9c9b989cc
Michael McNamara <mac@verilog.com>
2010-03-28 00:31:25 +0000

    Now that we correctly indent asserts, insure we never forget!
    git-svn-id: file:///verilog-mode-svn/trunk@570 db5bb42b-8904-0410-88e9-aa7026cf2ec3

--- tests/indent_assert_else.v
+++ tests/indent_assert_else.v
@@ -1,10 +1,10 @@
-module myassert(input clk, 
-		input reset, 
-		input [15:0] data);
+module myassert(input clk,
+                input        reset,
+                input [15:0] data);
    
    property myproperty;
       @(posedge clk)
-	$rose(reset) |-> data == 16'h0;
+        $rose(reset) |-> data  == 16'h0;
    endproperty
    
    //Assert, cover, and assume property statements
@@ -12,14 +12,39 @@ module myassert(input clk,
    //clause below is getting indented improperly.
    myassert0: assert property(myproperty) begin
       $display("myassert0 was successful");
-   end
-	      else begin
-		 $fatal("myassert0 was unsuccessful");
-	      end
-
+      a;
+      b;
+      c;
+      d;
+   end // myassert0: assert property (myproperty)
+   else begin
+      $fatal("myassert0 was unsuccessful");
+   end // else: !assert property(myproperty)
+   if (a) begin
+      b;
+      c;
+   end // if (a)
+   else begin
+      o;
+   end // else: !if(a)
+   assert (a) begin
+      o;
+   end // assert (a)
+   else begin
+      o;
+   end // else: !assert (a)
+   
+   assert (statement) begin
+      $display("assertion passed"); //this code is correctly indented
+   end // assert (statement)
+   else begin // this whole section should be moved to the left
+      $error("assertion failed");
+   end // else: !assert (statement)
+   
    //Also, any statements following the assert,
    //cover, and assume property statements get
    // indented too far to the right.
    always @(posedge clk) begin
-   end
+      a;
+   end // always @ (posedge clk)
 endmodule
