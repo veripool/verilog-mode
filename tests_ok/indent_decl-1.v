3a8b41a8df3690dbff6e1bf3e8a000edcfd17d87
Michael McNamara <mac@verilog.com>
2010-03-31 20:17:22 +0000

    Additional declaration tests.
    git-svn-id: file:///verilog-mode-svn/trunk@580 db5bb42b-8904-0410-88e9-aa7026cf2ec3

---
+++ tests_ok/indent_decl-1.v
@@ -0,0 +1,27 @@
+module soft_rst
+  (
+   // System clock and reset
+   input  clk,
+   input  rst_n,
+   
+   // Interface to software land
+   input  req_soft_rst, // Soft reset request
+   output soft_rst_dne, // Soft reset done
+   
+   // Interface to other modules
+   output dma_halt, // Reset pending, halt activity
+   input  tx_quiet, // TX side is dormant
+   input  rx_quiet, // RX side is dormant
+   input  c,
+   output soft_rst, // Soft (sync) reset to VC3 side
+   output hs_async_rst_n  // Async reset to host side
+   );
+
+   reg [1:0]    state;
+
+   reg [1:0] IDLE = 2'h0,
+     HALT  = 2'h1,
+     RST   = 2'h2,
+     DONE  = 2'h3;
+   
+endmodule // soft_rst
