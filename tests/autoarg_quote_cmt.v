b134a7622f074be057996e0715c36d3939f2efd8
Wilson Snyder <wsnyder@wsnyder.org>
2011-06-24 22:16:13 +0000

    Add test for bug360.
    git-svn-id: file:///verilog-mode-svn/trunk@697 db5bb42b-8904-0410-88e9-aa7026cf2ec3

---
+++ tests/autoarg_quote_cmt.v
@@ -0,0 +1,8 @@
+// bug360
+module f (/*AUTOARG*/);
+
+always @* r = "test/*";
+
+  input 				x;
+
+endmodule
---
+++ tests_ok/autoarg_quote_cmt.v
@@ -0,0 +1,11 @@
+// bug360
+module f (/*AUTOARG*/
+          // Inputs
+          x
+          );
+   
+   always @* r  = "test/*";
+   
+   input x;
+   
+endmodule
