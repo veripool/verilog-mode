d2150d86f67f076bd547d637a77128ecefa8d825
Michael McNamara <mac@verilog.com>
2011-12-11 07:20:02 +0000

    Change in lineup of expressions.
    git-svn-id: file:///verilog-mode-svn/trunk@747 db5bb42b-8904-0410-88e9-aa7026cf2ec3

--- tests_ok/autosense_smith.v
+++ tests_ok/autosense_smith.v
@@ -5,26 +5,26 @@ module x;
    // 16 registers max, register 15 is always the version number, so 15 are useable
    // NOTE: number_interface_regs must be the maximum (or 16 in this case) to get the version register
    parameter               NUMBER_INTERFACE_REGS  = 16;
-   parameter               MB_REG_START           = 3; // start at 4th register location 'h0010
-   parameter               CMD_REG_0_ADDR         = MB_REG_START;
-   parameter               CMD_REG_1_ADDR         = MB_REG_START+1;
-   parameter               CMD_REG_2_ADDR         = MB_REG_START+2;
-   parameter               CMD_REG_3_ADDR         = MB_REG_START+3;
-   parameter               CMD_REG_4_ADDR         = MB_REG_START+4;
-   parameter               CMD_REG_5_ADDR         = MB_REG_START+5;
-   parameter               CMD_REG_6_ADDR         = MB_REG_START+6;
-   parameter               CMD_REG_7_ADDR         = MB_REG_START+7;
-   parameter               CMD_REG_8_ADDR         = MB_REG_START+8;
-   parameter               CMD_REG_9_ADDR         = MB_REG_START+9;
-   parameter               CMD_REG_10_ADDR        = MB_REG_START+10;
+   parameter               MB_REG_START  = 3; // start at 4th register location 'h0010
+   parameter               CMD_REG_0_ADDR  = MB_REG_START;
+   parameter               CMD_REG_1_ADDR  = MB_REG_START+1;
+   parameter               CMD_REG_2_ADDR  = MB_REG_START+2;
+   parameter               CMD_REG_3_ADDR  = MB_REG_START+3;
+   parameter               CMD_REG_4_ADDR  = MB_REG_START+4;
+   parameter               CMD_REG_5_ADDR  = MB_REG_START+5;
+   parameter               CMD_REG_6_ADDR  = MB_REG_START+6;
+   parameter               CMD_REG_7_ADDR  = MB_REG_START+7;
+   parameter               CMD_REG_8_ADDR  = MB_REG_START+8;
+   parameter               CMD_REG_9_ADDR  = MB_REG_START+9;
+   parameter               CMD_REG_10_ADDR  = MB_REG_START+10;
    // mode regs
-   parameter               MODE_REG_0_ADDR        = CMD_REG_8_ADDR;
+   parameter               MODE_REG_0_ADDR  = CMD_REG_8_ADDR;
    
    // Absolute register 14 is Error counter
-   parameter               CMD_REG_14_ADDR        = 14;
-   parameter               CRC_ERROR_REG_ADDR     = CMD_REG_14_ADDR;
+   parameter               CMD_REG_14_ADDR  = 14;
+   parameter               CRC_ERROR_REG_ADDR  = CMD_REG_14_ADDR;
    // ------------ VERSION register is 15
-   parameter               VERSION_REG_ADDR       = 15;
+   parameter               VERSION_REG_ADDR  = 15;
    
    reg [NUMBER_INTERFACE_REGS-1:MB_REG_START] mb_reg_wr;
    reg [NUMBER_INTERFACE_REGS-1:MB_REG_START] mb_reg_rd;
