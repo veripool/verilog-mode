49e38dda6a2b9d4727d6e01ddf5bdc499a56ee89
Michael McNamara <mac@verilog.com>
2011-12-11 07:21:08 +0000

    Change in lineup of expressions.
    git-svn-id: file:///verilog-mode-svn/trunk@751 db5bb42b-8904-0410-88e9-aa7026cf2ec3

--- tests_ok/indent_dpi.v
+++ tests_ok/indent_dpi.v
@@ -2,8 +2,8 @@ module testbench;
    
    import "DPI-C" function void dpiWriteArray (input bit[7:0] data[]);
    import "DPI-C" pure function void dpiReadArray (output bit[7:0] data[]);
-      import "DPI-C" function void dpiAesSetKey ( int key_high_u int key_high_l,
-                                                  int key_low_u, int key_low_l );
+   import "DPI-C" function void dpiAesSetKey ( int key_high_u int key_high_l,
+                                               int key_low_u, int key_low_l );
    import "DPI-C" function void dpiAesSetIV (int iv_high_u, int iv_high_l,
                                              int iv_low_u, int iv_low_l);
    import "DPI-C" function void dpiAesSetSkip (int skip);
