7e4c34cb86eee127652cb6453874c06bfc37a4cc
Nikolay Puzanov <punzik@gmail.com>
2014-07-29 18:53:52 +0400

    Сode for electric apostrophe rewritten by Wilson Snyder remark

--- verilog-mode.el
+++ verilog-mode.el
@@ -111,7 +111,7 @@
 ;       verilog-auto-lineup              'declarations
 ;       verilog-highlight-p1800-keywords nil
 ;       verilog-linter			 "my_lint_shell_command"
-;       verilog-auto-bitwidth            t
+;       verilog-electric-apostrophe      t
 ;       )
 
 ;; 
@@ -712,11 +712,11 @@ The name of the function or case will be set between the braces."
   :type 'boolean)
 (put 'verilog-auto-endcomments 'safe-local-variable 'verilog-booleanp)
 
-(defcustom verilog-auto-bitwidth t
+(defcustom verilog-electric-apostrophe nil
   "Non-nil means automatically insert apostrophe after bitwidth."
   :group 'verilog-mode-actions
   :type 'boolean)
-(put 'verilog-auto-bitwidth 'safe-local-variable 'verilog-booleanp)
+(put 'verilog-electric-apostrophe 'safe-local-variable 'verilog-booleanp)
 
 (defcustom verilog-auto-delete-trailing-whitespace nil
   "Non-nil means to `delete-trailing-whitespace' in `verilog-auto'."
@@ -1334,16 +1334,16 @@ If set will become buffer local.")
     (define-key map "\C-c\C-e" 'verilog-expand-vector)
     (define-key map "\C-c\C-h" 'verilog-header)
 
-    (define-key map "b" 'electric-verilog-b)
-    (define-key map "B" 'electric-verilog-bb)
-    (define-key map "h" 'electric-verilog-h)
-    (define-key map "H" 'electric-verilog-hh)
-    (define-key map "d" 'electric-verilog-d)
-    (define-key map "D" 'electric-verilog-dd)
-    (define-key map "x" 'electric-verilog-x)
-    (define-key map "X" 'electric-verilog-xx)
-    (define-key map "s" 'electric-verilog-s)
-    (define-key map "S" 'electric-verilog-ss)
+    (define-key map "b" 'electric-verilog-apostrophe)
+    (define-key map "B" 'electric-verilog-apostrophe)
+    (define-key map "h" 'electric-verilog-apostrophe)
+    (define-key map "H" 'electric-verilog-apostrophe)
+    (define-key map "d" 'electric-verilog-apostrophe)
+    (define-key map "D" 'electric-verilog-apostrophe)
+    (define-key map "x" 'electric-verilog-apostrophe)
+    (define-key map "X" 'electric-verilog-apostrophe)
+    (define-key map "s" 'electric-verilog-apostrophe)
+    (define-key map "S" 'electric-verilog-apostrophe)
     
     map)
   "Keymap used in Verilog mode.")
@@ -3916,66 +3916,15 @@ With optional ARG, remove existing end of line comments."
 		 (kill-region (point) oldpnt)))))))
    (t (progn (insert "\t")))))
 
-;;
-;; Convert 3b to 3'b automatically.
-;; Copied from vlog-mode (http://vlog-mode.sourceforge.net/)
-;;
-(defun electric-verilog-bitwidth (c)
-  "Convert 3b to 3'b automatically."
-  (and verilog-auto-bitwidth
+;; From vlog-mode (http://vlog-mode.sourceforge.net/)
+(defun electric-verilog-apostrophe ()
+  "Insert apostrophe after bitwidth (e.g 1b -> 1'b)."
+  (interactive)
+  (and verilog-electric-apostrophe
        (looking-back "\\<[0-9]+")
+       (not (looking-back "\\\\[0-9]+"))
        (insert "'"))
-  (insert c))
-
-(defun electric-verilog-b ()
-  "Add possible ' before inserting b, for numbers."
-  (interactive)
-  (electric-verilog-bitwidth "b"))
-
-(defun electric-verilog-bb ()
-  "Add possible ' before inserting B, for numbers."
-  (interactive)
-  (electric-verilog-bitwidth "B"))
-
-(defun electric-verilog-h ()
-  "Add possible ' before inserting h, for numbers."
-  (interactive)
-  (electric-verilog-bitwidth "h"))
-
-(defun electric-verilog-hh ()
-  "Add possible ' before inserting H, for numbers."
-  (interactive)
-  (electric-verilog-bitwidth "H"))
-
-(defun electric-verilog-dd ()
-  "Add possible ' before inserting D, for numbers."
-  (interactive)
-  (electric-verilog-bitwidth "D"))
-
-(defun electric-verilog-d ()
-  "Add possible ' before inserting d, for numbers."
-  (interactive)
-  (electric-verilog-bitwidth "d"))
-
-(defun electric-verilog-x ()
-  "Add possible ' before inserting x, for numbers."
-  (interactive)
-  (electric-verilog-bitwidth "x"))
-
-(defun electric-verilog-xx ()
-  "Add possible ' before inserting X, for numbers."
-  (interactive)
-  (electric-verilog-bitwidth "X"))
-
-(defun electric-verilog-s ()
-  "Add possible ' before inserting s, for numbers (Verilog 2000)."
-  (interactive)
-  (electric-verilog-bitwidth "s"))
-
-(defun electric-verilog-ss ()
-  "Add possible ' before inserting S, for numbers (Verilog 2000)."
-  (interactive)
-  (electric-verilog-bitwidth "S"))
+  (insert last-input-event))
 
 ;;
 ;; Interactive functions
@@ -14035,7 +13984,7 @@ Files are checked based on `verilog-library-flags'."
        verilog-auto-tieoff-ignore-regexp
        verilog-auto-unused-ignore-regexp
        verilog-auto-wire-type
-       verilog-auto-bitwidth
+       verilog-electric-apostrophe
        verilog-before-auto-hook
        verilog-before-delete-auto-hook
        verilog-before-getopt-flags-hook
