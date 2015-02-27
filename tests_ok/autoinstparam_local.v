626a07d30b459e84baf3c9a028e9b2b4cfdd1e29
Wilson Snyder <wsnyder@wsnyder.org>
2015-02-26 21:00:41 -0500

    Test for last commit, bug889.

---
+++ tests_ok/autoinstparam_local.v
@@ -0,0 +1,26 @@
+//bug889
+
+module leaf #(
+              parameter   DATA_WIDTH = 256,
+              localparam  STRB_WIDTH_SHOULD_BE_HIDDEN = DATA_WIDTH/8 )
+   ();
+endmodule
+
+module test;
+   parameter integer DATA_WIDTH = 256;
+   parameter integer STRB_WIDTH = DATA_WIDTH/8;
+   
+   /* leaf AUTO_TEMPLATE (
+    .DATA_WIDTH( DATA_WIDTH ),
+    .STRB_WIDTH_SHOULD_BE_HIDDEN ( STRB_WIDTH ),
+    .\(.*\) ( \1 ),
+    );*/
+   
+   leaf
+     #( /*AUTOINSTPARAM*/
+        // Parameters
+        .DATA_WIDTH                     ( DATA_WIDTH ))          // Templated
+   u_leaf
+     ( /*AUTOINST*/);
+   
+endmodule
