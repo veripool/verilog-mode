742b54b4c743678cd1e3f601e2e815baa80d4393
Alex Reed <acreed4@gmail.com>
2015-07-28 10:04:42 -0400

    Fix indentation after 'assert final' statements.
    * verilog-mode.el (verilog-calc-1,
      verilog-in-deferred-immediate-final-p):  When the keyword 'final'
      follows 'assert/assume/cover', then it is part of a deferred immediate
      assertion item and should not be treated as a final construct for
      indentation.  Reported by Yuri Sugihara.

---
+++ tests_ok/indent_immediate_assertion.sv
@@ -0,0 +1,27 @@
+// Issue 949 - Indenting SVA assert final block broken
+module tb;
+   begin
+      a0: assert final (data0 == 1) else
+        $error;
+   end
+endmodule
+
+
+// Now check for other types of immediate assertion
+module tb;
+   always @(abcd) begin
+      
+      // simple immediate assert statement
+      assert (xyz) a  = b;
+      
+      // deferred immediate cover statement w/ #0
+      if(x)
+        cover #0 (efg)
+          $display("covered");
+      
+      // deferred immedate assume statement w/ final
+      assume final (abcd) else
+        $display();
+   end
+   
+endmodule
