8d5c3f8fad992a06dc3c7bfc73723cebe2af6fe8
Wilson Snyder <wsnyder@wsnyder.org>
2018-09-11 10:34:56 -0400

    Fix testcase, bug1341.

--- tests/indent_virtual_class.sv
+++ tests/indent_virtual_class.sv
@@ -1,5 +1,5 @@
 // issue 928 - 'pure virtual' items in classes break indentation
-package p:
+package p;
 virtual class c extends parent;
 pure virtual protected task t(type_t a, type_t b);
 function f(string s="");
