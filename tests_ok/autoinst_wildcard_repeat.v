47b88f2571cf31c55e4a23083dedc522a1eb0562
Wilson Snyder <wsnyder@wsnyder.org>
2019-01-02 18:00:46 -0500

    Fix AUTO_TEMPLATE with regexp capture group reference, but1379.
    	* verilog-mode.el (verilog-read-auto-template-middle): Fix
    	AUTO_TEMPLATE with regexp capture group reference, but1379.
    	Reported by David Rogoff.

---
+++ tests_ok/autoinst_wildcard_repeat.v
@@ -0,0 +1,21 @@
+module autoinst_wildcard_repeat;
+
+   /* sub AUTO_TEMPLATE (
+    .w_\(.*\)_\1   (match_\1),
+    .w_\(.*\)   (hit_\1),
+    ); */
+
+   sub sub
+     (/*AUTOINST*/
+      // Inputs
+      .w_a_a				(match_a),		 // Templated
+      .w_bb_bb				(match_bb),		 // Templated
+      .w_x_y				(hit_x_y));		 // Templated
+
+endmodule
+
+module sub;
+   input w_a_a;
+   input w_bb_bb;
+   input w_x_y;
+endmodule
