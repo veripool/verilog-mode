be9b5afdbc226eed07ac1ad5fd9bc7f6accaa387
Wilson Snyder <wsnyder@wsnyder.org>
2019-08-27 13:38:31 -0400

    Fix `verilog-goto-defun' to find automatic function/tasks, bug1492.
    	* verilog-mode.el (verilog-build-defun-re): Fix
    	`verilog-goto-defun' to find automatic function/tasks, bug1492.
    	Reported by Enze Chi.

--- verilog-mode.el
+++ verilog-mode.el
@@ -7627,9 +7627,9 @@ and `verilog-separator-keywords'.)"
 (defun verilog-build-defun-re (str &optional arg)
   "Return function/task/module starting with STR as regular expression.
 With optional second ARG non-nil, STR is the complete name of the instruction."
-  (if arg
-      (concat "^\\(function\\|task\\|module\\)[ \t]+\\(" str "\\)\\>")
-    (concat "^\\(function\\|task\\|module\\)[ \t]+\\(" str "[a-zA-Z0-9_]*\\)\\>")))
+  (unless arg
+    (setq str (concat str "[a-zA-Z0-9_]*")))
+  (concat "^\\s-*\\(function\\|task\\|module\\)[ \t]+\\(?:\\(?:static\\|automatic\\)\\s-+\\)?\\(" str "\\)\\>"))
 
 (defun verilog-comp-defun (verilog-str verilog-pred verilog-flag)
   "Function passed to `completing-read', `try-completion' or `all-completions'.
