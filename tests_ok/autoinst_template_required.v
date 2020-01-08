f65bdff5a7cdbba2bd45bf183078fe6737bf0b0a
Wilson Snyder <wsnyder@wsnyder.org>
2020-01-08 06:28:42 -0500

    Add `verilog-auto-inst-template-required' to only insert AUTOINST ports inside an AUTO_TEMPLATE, msg3170.
    * verilog-mode.el (verilog-auto-inst-template-required)
    (verilog-auto-inst): Add `verilog-auto-inst-template-required' to
    only insert AUTOINST ports inside an AUTO_TEMPLATE,
    msg3170. Reported by Ted Huang, Brian Magnuson.

---
+++ tests_ok/autoinst_template_required.v
@@ -0,0 +1,29 @@
+// This file ONLY is placed into the Public Domain, for any use,
+// without warranty, 2020 by Wilson Snyder.
+
+module autoinst_multitemplate ();
+   
+   /*
+    SubB AUTO_TEMPLATE (
+    .b (Boo@),
+    .other (Other),
+    );*/
+   
+   SubB subb (/*AUTOINST*/
+              // Inputs
+              .b                        (Boo),                   // Templated
+              .other                    (Other));                // Templated
+   
+   SubC subc (/*AUTOINST*/);
+   
+endmodule
+
+module SubB (input not_listed, input b, input other);
+endmodule
+
+module SubC (input also_not_listed, input b);
+endmodule
+
+// Local Variables:
+// verilog-auto-inst-template-required: t
+// End:
