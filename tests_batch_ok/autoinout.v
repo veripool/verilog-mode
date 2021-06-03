e3d5be923cc1cc6b79b1acfc5df3324372a64f24
TAKAI Kousuke <62541129+t-a-k@users.noreply.github.com>
2021-06-04 06:00:03 +0900

    Fix leaving extra spaces before tabs on lining up declarations. (#1723)
    * verilog-mode.el (verilog-indent-declaration, verilog-pretty-declarations):
    Reported by TAKAI Kousuke.

--- tests_batch_ok/autoinout.v
+++ tests_batch_ok/autoinout.v
@@ -60,8 +60,8 @@ module instio (/*AUTOARG*/
    inout  sec_io;
    output sec_out;
 
-   wire   lower_out = lower_ina | lower_io;
-   wire   sec_out = sec_ina | sec_io;
+   wire	  lower_out = lower_ina | lower_io;
+   wire	  sec_out = sec_ina | sec_io;
 
 endmodule
 
