b6d4104572d6548b9227e88ac8b7237b4d2789a0
Dan McMahill <dmcmahill@butterflynetinc.com>
2021-09-01 09:49:20 -0400

    Support Verilog-A `analog` block (#1738)
    * verilog-mode.el (verilog-basic-complete-re)
    (verilog-behavioral-block-beg-re, verilog-defun-keywords)
    (verilog-defun-level-generate-only-re, verilog-defun-level-re)
    (verilog-endcomment-reason-re, verilog-indent-re)
    (verilog-keywords, verilog-no-indent-begin-re)
    (verilog-set-auto-endcomments): Support Verilog-A `analog` blocks (#1738).
    Reported by Dan McMahill.

---
+++ tests/label_analog.v
@@ -0,0 +1,14 @@
+module mymodule();
+    parameter real fc=10e6;
+  parameter real bw=25e3;
+
+analog begin
+// contents of module here
+   end
+
+     endmodule
+
+// Local Variables:
+// verilog-minimum-comment-distance: 1
+// verilog-auto-endcomments: t
+// End:
---
+++ tests_ok/label_analog.v
@@ -0,0 +1,14 @@
+module mymodule();
+   parameter real fc=10e6;
+   parameter real bw=25e3;
+   
+   analog begin
+      // contents of module here
+   end // analog
+   
+endmodule // mymodule
+
+// Local Variables:
+// verilog-minimum-comment-distance: 1
+// verilog-auto-endcomments: t
+// End:
