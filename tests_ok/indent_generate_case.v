af28cc3c2dc2abd6d8403bc255116f88a4515aa6
Nikolay Puzanov <np@embddr.com>
2021-09-16 14:33:21 +0300

    Fix indentation within generate construct after always (#1404,#1257)
    * verilog-mode.el (verilog-in-generate-region-p): Add 'if', 'case' and 'for' keywords for generate block detection.
    * Add indentation tests: indent_generate_if.v, indent_generate_case.v and indent_generate_for.v
    * Fix indentation in test autoreset_inf_bug325.v

---
+++ tests_ok/indent_generate_case.v
@@ -0,0 +1,24 @@
+module indent_gen_case #(parameter P = 0)
+   (input d, output reg q);
+   
+   case (P)
+     0: always @(*)
+       q = d;
+     
+     1: begin
+        always @(*)
+          q = d;
+     end
+     
+     2: always @(*) begin
+        q = d;
+     end
+     
+     3: begin
+        always @(*) begin
+           q = d;
+        end
+     end
+   endcase
+   
+endmodule
---
+++ tests_ok/indent_generate_if.v
@@ -0,0 +1,24 @@
+module indent_gen_if #(parameter P = 0)
+   (input d, output reg q);
+   
+   if (P == 0) begin
+      always @(*)
+        q = d;
+   end
+   
+   if (P == 1) begin
+      always @(*) begin
+         q = d;
+      end
+   end
+   
+   if (P == 1) begin
+      always @(*)
+        q = d;
+   end
+   else begin
+      always @(*)
+        q = d + 1'b1;
+   end
+   
+endmodule
