30c106c0cf31f7dd5d12107307d8c5c6b731fc29
Stefan Monnier <monnier@iro.umontreal.ca>
2021-09-16 10:15:13 -0400

    Prepare for lexical bindings.
    * verilog-mode.el (verilog-do-indent)
    (verilog-indent-declaration, verilog-read-always-signals-recurse)
    (verilog-read-decls): Prepare for lexical bindings.

--- tests_ok/autoinst_instname_carlh.v
+++ tests_ok/autoinst_instname_carlh.v
@@ -1,5 +1,7 @@
 module ex;
    
+   // Note "inst" below should be vl-inst, but allowed for historical usage
+   
    /* autoinst_paramover_sub AUTO_TEMPLATE (
     .a(@"vl-cell-name"_in[]),
     .b(@"(substring inst 2)"_out[]),
