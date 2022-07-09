4bea1481c482b2a2bda2f80dd6eba4d30630fd6f
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2022-07-09 17:57:16 +0200

    Reproduce issue #1760 if indent-tabs-mode is set to nil

---
+++ tests/indent_decl_1760.sv
@@ -0,0 +1,35 @@
+module test (
+input logic a,
+output logic b
+);
+
+for (genvar i=0; i<2; i++) begin : g_slice
+
+submod
+u_sub_0 #(
+.RESET_POL (RESET_POL)
+)(
+.clk     (clk),
+.reset_n (reset_n),
+.d       (d[i]),
+.q       (q_0[i])
+);
+
+submod
+u_sub_1 #(
+.RESET_POL (RESET_POL)
+)(
+.clk     (clk),
+.reset_n (reset_n),
+.d       (q_0[i]),
+.q       (q[i])
+);
+
+end
+
+endmodule
+
+
+// Local Variables:
+// indent-tabs-mode: nil
+// End:
