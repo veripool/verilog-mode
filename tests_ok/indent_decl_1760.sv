9901882a8ed008fec3f8ef556ccd28090e509229
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2022-07-11 19:30:29 +0200

    Tests: Update #1760 golden after merging #1782.
    Signed-off-by: Gonzalo Larumbe <gonzalomlarumbe@gmail.com>

--- tests_ok/indent_decl_1760.sv
+++ tests_ok/indent_decl_1760.sv
@@ -6,14 +6,14 @@ module test (
    for (genvar i=0; i<2; i++) begin : g_slice
       
       submod
-                 u_sub_0 #(
-                           .RESET_POL (RESET_POL)
-                           )(
-                             .clk     (clk),
-                             .reset_n (reset_n),
-                             .d       (d[i]),
-                             .q       (q_0[i])
-                             );
+        u_sub_0 #(
+                  .RESET_POL (RESET_POL)
+                  )(
+                    .clk     (clk),
+                    .reset_n (reset_n),
+                    .d       (d[i]),
+                    .q       (q_0[i])
+                    );
       
       submod
         u_sub_1 #(
