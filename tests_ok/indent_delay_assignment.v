88030f23db7eba3a6ee98694beca12f643567b4e
He Yun <my2817@163.com>
2024-11-07 21:00:37 +0800

    Imporve ident of delay control assignments (#1883)
    (verilog-do-indent) Imporve ident of delay control assignments (#1883)

---
+++ tests_ok/indent_delay_assignment.v
@@ -0,0 +1,14 @@
+module a();
+   always(*)begin
+      a = #1 b |
+             c |
+             d;
+      a <= #1  b |
+               c |
+               d;
+      a <= # 1  b |
+                c |
+                d;
+      
+   end
+endmodule
