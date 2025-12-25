54baf3bb098326ef93d7777e53c3040327a63b58
anythingelse0 <nothingelse@live.com>
2025-12-25 11:40:37 +0800

    Refactor input declarations and template parameters

--- tests_ok/autoinst_param_2d.v
+++ tests_ok/autoinst_param_2d.v
@@ -11,9 +11,9 @@ module a();
    input [AUM-1:0]          my_data_av[VUM];
    input [AUM-1:0][BUM-1:0] my_data_ab;
    input [AUM-1:0][BUM-1:0] my_data_abv[VUM];
-   
+
    input [XUM-1:0][YUM-1:0] my_data_xyz[ZUM];
-   
+
    input PARAMS0__t         params0 [1:0];
    input PARAMS1__t         params1 [1:0];
 endmodule
@@ -27,28 +27,28 @@ module top (/*AUTOARG*/
             );
    /*AUTOINPUT*/
    // Beginning of automatic inputs (from unused autoinst inputs)
-   input [TEST0_AUM-1:0] [TEST0_BUM-1:0] TEST0_my_data_ab;              // To a_0 of a.v
-   input [TEST0_AUM-1:0] [TEST0_BUM-1:0] TEST0_my_data_abv [TEST0_VUM]; // To a_0 of a.v
-   input [TEST0_AUM-1:0]                 TEST0_my_data_av [TEST0_VUM];  // To a_0 of a.v
-   input                                 TEST0_my_data_v [VUM];         // To a_0 of a.v
-   input                                 TEST0_my_data_vv [VUM][V2];    // To a_0 of a.v
-   input [XUM-1:0] [YUM-1:0]             TEST0_my_data_xyz [ZUM];       // To a_0 of a.v
-   input                                 TEST0_my_data_z;               // To a_0 of a.v
-   input PARAMS0__t                      TEST0_params0 [1:0];           // To a_0 of a.v
-   input PARAMS1__t                      TEST0_params1 [1:0];           // To a_0 of a.v
-   input [TEST1_AUM-1:0] [TEST1_BUM-1:0] TEST1_my_data_ab;              // To a_1 of a.v
-   input [TEST1_AUM-1:0] [TEST1_BUM-1:0] TEST1_my_data_abv [TEST1_VUM]; // To a_1 of a.v
-   input [TEST1_AUM-1:0]                 TEST1_my_data_av [TEST1_VUM];  // To a_1 of a.v
-   input                                 TEST1_my_data_v [VUM];         // To a_1 of a.v
-   input                                 TEST1_my_data_vv [VUM][V2];    // To a_1 of a.v
-   input [XUM-1:0] [YUM-1:0]             TEST1_my_data_xyz [ZUM];       // To a_1 of a.v
-   input                                 TEST1_my_data_z;               // To a_1 of a.v
-   input PARAMS0__t                      TEST1_params0 [1:0];           // To a_1 of a.v
-   input PARAMS1__t                      TEST1_params1 [1:0];           // To a_1 of a.v
+   input [TEST0_AUM-1:0] [TEST0_BUM-1:0] TEST0_my_data_ab;                       // To a_0 of a.v
+   input [TEST0_AUM-1:0] [TEST0_BUM-1:0] TEST0_my_data_abv [TEST0_VUM];          // To a_0 of a.v
+   input [TEST0_AUM-1:0]                 TEST0_my_data_av [TEST0_VUM];           // To a_0 of a.v
+   input                                 TEST0_my_data_v [TEST0_VUM];            // To a_0 of a.v
+   input                                 TEST0_my_data_vv [TEST0_VUM][TEST0_V2]; // To a_0 of a.v
+   input [XUM-1:0] [YUM-1:0]             TEST0_my_data_xyz [ZUM];                // To a_0 of a.v
+   input                                 TEST0_my_data_z;                        // To a_0 of a.v
+   input PARAMS0__t                      TEST0_params0 [1:0];                    // To a_0 of a.v
+   input PARAMS1__t                      TEST0_params1 [1:0];                    // To a_0 of a.v
+   input [TEST1_AUM-1:0] [TEST1_BUM-1:0] TEST1_my_data_ab;                       // To a_1 of a.v
+   input [TEST1_AUM-1:0] [TEST1_BUM-1:0] TEST1_my_data_abv [TEST1_VUM];          // To a_1 of a.v
+   input [TEST1_AUM-1:0]                 TEST1_my_data_av [TEST1_VUM];           // To a_1 of a.v
+   input                                 TEST1_my_data_v [TEST1_VUM];            // To a_1 of a.v
+   input                                 TEST1_my_data_vv [TEST1_VUM][TEST1_V2]; // To a_1 of a.v
+   input [XUM-1:0] [YUM-1:0]             TEST1_my_data_xyz [ZUM];                // To a_1 of a.v
+   input                                 TEST1_my_data_z;                        // To a_1 of a.v
+   input PARAMS0__t                      TEST1_params0 [1:0];                    // To a_1 of a.v
+   input PARAMS1__t                      TEST1_params1 [1:0];                    // To a_1 of a.v
    // End of automatics
    /*AUTOOUTPUT*/
    /*AUTOWIRE*/
-   
+
    /*
     a AUTO_TEMPLATE
     (
@@ -64,16 +64,16 @@ module top (/*AUTOARG*/
    a_0 (/*AUTOINST*/
         // Inputs
         .my_data_z                      (TEST0_my_data_z),       // Templated
-        .my_data_v                      (TEST0_my_data_v/*.[VUM]*/), // Templated
-        .my_data_vv                     (TEST0_my_data_vv/*.[VUM][V2]*/), // Templated
+        .my_data_v                      (TEST0_my_data_v/*.[TEST0_VUM]*/), // Templated
+        .my_data_vv                     (TEST0_my_data_vv/*.[TEST0_VUM][TEST0_V2]*/), // Templated
         .my_data_av                     (TEST0_my_data_av/*[TEST0_AUM-1:0].[TEST0_VUM]*/), // Templated
         .my_data_ab                     (TEST0_my_data_ab/*[TEST0_AUM-1:0][TEST0_BUM-1:0]*/), // Templated
         .my_data_abv                    (TEST0_my_data_abv/*[TEST0_AUM-1:0][TEST0_BUM-1:0].[TEST0_VUM]*/), // Templated
         .my_data_xyz                    (TEST0_my_data_xyz/*[XUM-1:0][YUM-1:0].[ZUM]*/), // Templated
         .params0                        (TEST0_params0/*.[1:0]*/), // Templated
         .params1                        (TEST0_params1/*.[1:0]*/)); // Templated
-   
-   
+
+
    a #(/*AUTOINSTPARAM*/
        // Parameters
        .AUM                             (TEST1_AUM),             // Templated
@@ -83,15 +83,15 @@ module top (/*AUTOARG*/
    a_1 (/*AUTOINST*/
         // Inputs
         .my_data_z                      (TEST1_my_data_z),       // Templated
-        .my_data_v                      (TEST1_my_data_v/*.[VUM]*/), // Templated
-        .my_data_vv                     (TEST1_my_data_vv/*.[VUM][V2]*/), // Templated
+        .my_data_v                      (TEST1_my_data_v/*.[TEST1_VUM]*/), // Templated
+        .my_data_vv                     (TEST1_my_data_vv/*.[TEST1_VUM][TEST1_V2]*/), // Templated
         .my_data_av                     (TEST1_my_data_av/*[TEST1_AUM-1:0].[TEST1_VUM]*/), // Templated
         .my_data_ab                     (TEST1_my_data_ab/*[TEST1_AUM-1:0][TEST1_BUM-1:0]*/), // Templated
         .my_data_abv                    (TEST1_my_data_abv/*[TEST1_AUM-1:0][TEST1_BUM-1:0].[TEST1_VUM]*/), // Templated
         .my_data_xyz                    (TEST1_my_data_xyz/*[XUM-1:0][YUM-1:0].[ZUM]*/), // Templated
         .params0                        (TEST1_params0/*.[1:0]*/), // Templated
         .params1                        (TEST1_params1/*.[1:0]*/)); // Templated
-   
+
 endmodule
 
 // Local Variables:
