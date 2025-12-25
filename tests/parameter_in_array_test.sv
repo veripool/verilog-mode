eebfca0b104afc5dd69157ea90e0a20f43afc176
anythingelse0 <nothingelse@live.com>
2025-12-25 13:54:09 +0800

    test in 1-D unpacked array

---
+++ tests/parameter_in_array_test.sv
@@ -0,0 +1,38 @@
+module parameter_in_array_test
+#(
+    parameter TEST = 6
+)
+(
+
+   output port_out_array1 [TEST], // FIX  bug in 1-D unpacked array
+   output [TEST+2-1:0] port_out_array2 [TEST+2],
+   output [TEST+3-1:0] [TEST+3:0]port_out_array3 ,
+
+   input port_in_array1 [TEST+1],
+   input [TEST+2-1:0] port_in_array2 [TEST+2] ,
+   input [TEST+3-1:0] [TEST+3:0]port_in_array3 ,
+
+)
+
+
+endmodule
+
+
+module test()
+/*AUTOWIRE*/
+/*AUTOREG*/
+
+    parameter_in_array_test
+    #(
+        .TEST(8)
+    )
+    u_parameter_in_array_test
+    (
+    /*AUTOINST*/);
+endmodule
+// Local Variables:
+// verilog-auto-inst-param-value:t
+// fill-column: 1
+// End:
+
+
